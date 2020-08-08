6ac4e07dc2f974085c8dd14a6a6808eeac848db3
jiye <jiyecafe@gmail.com>
2020-08-08 09:16:18 +0800

    fix

--- keyjoker.user.js
+++ keyjoker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         KeyJoker Auto Task
-// @namespace    KeyJokerjAutoTask
+// @namespace    KeyJokerAutoTask
 // @version      0.8.5
 // @description  KeyJoker Auto Task,修改自https://greasyfork.org/zh-CN/scripts/383411
 // @author       祭夜
